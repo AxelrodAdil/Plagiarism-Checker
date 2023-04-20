870252f440a6bcc8136e244ae57ae95263cb79cf
Adil Myktybek <taraza309@gmail.com>
2023-04-21 02:48:15 +0600

    [AM-AntiPlagiarism] - Updated front-code (css)

--- src/main/resources/static/js/requestsender.js
+++ src/main/resources/static/js/requestsender.js
@@ -62,52 +62,107 @@ form.addEventListener('submit', event => {
 
             // Create a new HTML page with the result
             const newPage = `
-      <html>
-        <head>
-          <title>Plagiarism Checker Result</title>
-          <style>
-            .result-container {
-              margin: 50px;
-              padding: 20px;
-              border: 2px solid #ccc;
-              border-radius: 5px;
-            }
-            .result-container p {
-              margin: 0;
-            }
-            .result-container h2 {
-              margin-top: 0;
-            }
-            .urls-container {
-              display: none;
-              margin-top: 10px;
-              border: 1px solid #ccc;
-              padding: 5px;
-            }
-          </style>
-        </head>
-        <body>
-          <div class="result-container">
-            <h2>Plagiarism Checker Result</h2>
-            <p>Plagiarism percentage: <strong>${data.percentage.toFixed(2)}%</strong></p>
-            <p>Click below to view the URLs:</p>
-            <button onclick="toggleUrls()">View URLs</button>
-            <div class="urls-container" id="urls-container">
-              <p>Similar URLs:</p>
-              <ul>
-                ${data.urls.map(url => `<li><a href="${url}" target="_blank">${url}</a></li>`).join('')}
-              </ul>
-            </div>
-          </div>
-          <script>
-            function toggleUrls() {
-              const urlsContainer = document.querySelector('#urls-container');
-              urlsContainer.style.display = urlsContainer.style.display === 'none' ? 'block' : 'none';
-            }
-          </script>
-        </body>
-      </html>
-    `;
+            <html>
+                <head>
+                  <title>Plagiarism Checker Result</title>
+                  <style>
+                    body {
+                      font-family: Arial, sans-serif;
+                      margin: 0;
+                      padding: 0;
+                      background-color: #f5f3f8;
+                    }
+                    h2 {
+                      font-size: 36px;
+                      text-align: center;
+                      margin-top: 50px;
+                      color: #6d428c;
+                    }
+                    .result-container {
+                      margin: 50px auto;
+                      max-width: 600px;
+                      padding: 20px;
+                      border: 2px solid #ccc;
+                      border-radius: 5px;
+                      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                      text-align: center;
+                      background-color: #fff;
+                    }
+                    .result-container p {
+                      margin: 0;
+                      color: #6d428c;
+                      font-size: 18px;
+                    }
+                    strong {
+                      color: #8a4baf;
+                      font-weight: bold;
+                    }
+                    .urls-container {
+                      display: none;
+                      margin-top: 10px;
+                      border: 1px solid #ccc;
+                      padding: 5px;
+                    }
+                    .urls-container p {
+                      margin-bottom: 10px;
+                      color: #6d428c;
+                      font-size: 18px;
+                    }
+                    ul {
+                      margin: 0;
+                      padding: 0;
+                      list-style: none;
+                    }
+                    li {
+                      margin-bottom: 5px;
+                      font-size: 16px;
+                    }
+                    a {
+                      color: #8a4baf;
+                      text-decoration: none;
+                    }
+                    button {
+                      background-color: #8a4baf;
+                      color: #fff;
+                      padding: 10px 20px;
+                      border: none;
+                      border-radius: 5px;
+                      font-size: 16px;
+                      cursor: pointer;
+                      margin-top: 20px;
+                    }
+                    button:hover {
+                      background-color: #6d428c;
+                    }
+                  </style>
+                </head>
+                <body>
+                <div class="result-container">
+                  <h2>Plagiarism Checker Result</h2>
+                  <p>Plagiarism percentage: <strong>${data.percentage.toFixed(2)}%</strong></p>
+                  <p>Click below to view the URLs:</p>
+                  <button onclick="toggleUrls()">View URLs</button>
+                  <div class="urls-container" id="urls-container">
+                    <p>Similar URLs:</p>
+                    <ul>
+                      ${data.urls
+                                .map(
+                                    (url) => `<li><a href="${url}" target="_blank">${url}</a></li>`
+                                )
+                                .join("")}
+                    </ul>
+                  </div>
+                </div>
+                <script>
+                  function toggleUrls() {
+                    const urlsContainer = document.querySelector("#urls-container");
+                    urlsContainer.style.display =
+                            urlsContainer.style.display === "none" ? "block" : "none";
+                  }
+                </script>
+                </body>
+                </html>
+            `;
     // Create a Blob object with the new HTML page
     const blob = new Blob([newPage], { type: 'text/html' });
 
